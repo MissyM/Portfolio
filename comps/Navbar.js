30ee006866453e9f4bf113302ced4eda059662d5
Milena <mcabrerapatino@gmail.com>
2021-07-01 08:33:08 -0500

    feat: first commit

---
+++ comps/Navbar.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+const Navbar = () => {
+ return (
+  <nav>
+   <div className="logo">
+    <h1>Ninja List</h1>
+   </div>
+   <a>Home</a>
+   <a>About</a>
+   <a>Ninja Listing</a>
+  </nav>
+  );
+}
+
+export default Navbar; 
---
+++ pages/about.js
@@ -0,0 +1,9 @@
+const About = () => {
+ return (<div>
+  <h1>About</h1>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?</p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?</p>
+ </div>);
+}
+
+export default About;
